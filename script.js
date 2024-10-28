de63ad2ba11864c50bc6ba9b3780d9823443fe58
nisaninan21 <nisaninan1234@gmail.com>
2024-10-28 21:35:40 +0530

    First branch commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const question = [
+    {
+        question: "Which is largest animal in the world?",
+        answers:[
+            {text : "Shark", correct: false},
+            {text : "Blue whale", correct: true},
+            {text : "Elephant", correct: false},
+            {text : "Giraffe", correct: false},
+        ]
+    }
+]
